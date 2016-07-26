6c34fce215ba5879e9e50e07eeb2b1aba8341437
pntan <pntan@aavn-ws-208.aavn.local>
2016-07-26 15:40:49 +0700

    Add more tariff files

--- src/main/java/com/aiza/journey/utils/TariffChecking.java
+++ src/main/java/com/aiza/journey/utils/TariffChecking.java
@@ -5,34 +5,57 @@ import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Comparator;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class TariffChecking {
+	private static final String TARIFF_C0_PLUS = "C0+";
+	private static final String TARIFF_B2Y = "B2Y";
 	private static final String FILE_URL_OF_OW_2013 = "src/main/resources/tar13ow.txt";
+	private static final String FILE_URL_OF_LU_2013 = "src/main/resources/tar13lu.txt";
+	private static final String FILE_URL_OF_OW_2016 = "src/main/resources/tar16ow.txt";
 
 	public static void main(String[] args) throws IOException {
 		TariffChecking checkTariff = new TariffChecking();
-		BigDecimal taxableIncome = new BigDecimal(113000);
-		String tariffCode = "B2+";
-		List<String> tariffCodeMatchedLineList = checkTariff.getCorrespondingListFromTariffCode(tariffCode);
-		List<TariffValue> taxableIncomeRange = checkTariff.decodeLineStrToTaxableIncome(tariffCodeMatchedLineList, taxableIncome); //new BigDecimal(110000)
-		taxableIncomeRange.forEach(k -> {
-			System.out.println(k);
-		});
 		
+		// Handle for 2013, canton ow
+		BigDecimal taxableIncome = new BigDecimal(14000);
+		TariffValue nearestOrEqualTariffValue = checkTariff.getNearestOrEqualTariffValue("B2+", taxableIncome, FILE_URL_OF_OW_2013);
+		System.out.println("OW TaxableIncome to get rate: " + taxableIncome + " Nearest taxable value: "
+				+ nearestOrEqualTariffValue.getTaxableIncome() + " --> rate: " + nearestOrEqualTariffValue.getRate());
+		
+		// Hanlde for 2013, canton lu for tariff code: C0d
+		BigDecimal taxableIncomeLu = new BigDecimal(8200);
+		TariffValue nearestOrEqualTariffValue2 = checkTariff.getNearestOrEqualTariffValue("C0d", taxableIncomeLu, FILE_URL_OF_LU_2013);
+		System.out.println("LU TaxableIncome to get rate: " + taxableIncomeLu + " Nearest taxable value: "
+				+ nearestOrEqualTariffValue2.getTaxableIncome() + " --> rate: " + nearestOrEqualTariffValue2.getRate());
+		
+		// Hanlde for 2013, canton lu for tariff code: C0+
+		BigDecimal taxableIncomeLuDiffTariffCode = new BigDecimal(5000);
+		TariffValue nearestOrEqualTariffValue3 = checkTariff.getNearestOrEqualTariffValue(TARIFF_C0_PLUS, taxableIncomeLuDiffTariffCode, FILE_URL_OF_LU_2013);
+		System.out.println("LU 2013 for tariff " + TARIFF_C0_PLUS + " TaxableIncome to get rate: " + taxableIncomeLuDiffTariffCode + " Nearest taxable value: "
+				+ nearestOrEqualTariffValue3.getTaxableIncome() + " --> rate: " + nearestOrEqualTariffValue3.getRate());
+		
+		// Handle for 2016, canton lu for tariff code: B2Y
+		BigDecimal taxableIncomeLu2016 = new BigDecimal(14000);
+		TariffValue nearestOrEqualTariffValue4 = checkTariff.getNearestOrEqualTariffValue(TARIFF_B2Y, taxableIncomeLu2016, FILE_URL_OF_OW_2016);
+		System.out.println("OW 2016 for tariff " + TARIFF_B2Y + " TaxableIncome to get rate: " + taxableIncomeLu2016 + " Nearest taxable value: "
+				+ nearestOrEqualTariffValue4.getTaxableIncome() + " --> rate: " + nearestOrEqualTariffValue4.getRate());
+	}
+	
+	public TariffValue getNearestOrEqualTariffValue(String tariffCode, BigDecimal taxableIncome, String fileUrl) throws IOException {
+		List<String> tariffCodeMatchedLineList = getCorrespondingListFromTariffCode(tariffCode, fileUrl);
+		List<TariffValue> taxableIncomeRange = decodeLineStrToTaxableIncome(tariffCodeMatchedLineList, taxableIncome);
 		TariffValue nearestOrEqualTariffValue = taxableIncomeRange.stream()
 			.max(Comparator.comparing(TariffValue::getTaxableIncome)).get();
-		System.out.println(nearestOrEqualTariffValue.getTaxableIncome() + " --> rate: " + nearestOrEqualTariffValue.getRate());
+		return nearestOrEqualTariffValue;
 	}
 
-	public List<String> getCorrespondingListFromTariffCode(String tariffCodes) throws IOException {
-		return Files.lines(Paths.get(FILE_URL_OF_OW_2013))
+	public List<String> getCorrespondingListFromTariffCode(String tariffCode, String fileUrl) throws IOException {
+		return Files.lines(Paths.get(fileUrl))
 				.parallel() // for parallel processing
 				.filter(line -> line.length() > 2)
-				.filter(line -> line.toLowerCase().contains(tariffCodes.toLowerCase()))
+				.filter(line -> line.toLowerCase().contains(tariffCode.toLowerCase()))
 				.collect(Collectors.toList());
 	}
 	
